555b2d7f18e67c940cdac146faf50dbd4681ae15
saonguyen <saonguyen.hust@gmail.com>
2022-06-29 22:29:04 +0700

    bt7_6

---
+++ Bai_tap/bt7_6/javascript.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputLength;
+inputWidth= prompt("Enter the width");
+inputLength= prompt("Enter the length");
+let width = parseInt(inputWidth);
+let length = parseInt(inputLength);
+let area = width*length;
+document.write("this area is:=" + area);
